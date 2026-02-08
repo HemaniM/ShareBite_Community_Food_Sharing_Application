e3f0885ec1bad1ac59d2395b44ef5f4f40007a03
HarshMaurya04 <harshm8540@gmail.com>
2026-02-08 18:00:10 +0530

    resolve tailwind configuration

--- Frontend/src/Routes.jsx
+++ Frontend/src/Routes.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-// Import page components
-import LoginPage from './pages/Login';
-import Homepage from './pages/Homepage';
+// Pages
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Auth/Login";
+import Signup from "./pages/Auth/Signup";
+import AuthLayout from "./pages/Auth/AuthLayout";
 
 const AppRoutes = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      {/* Auth routes (START of website) */}
+      <Route element={<AuthLayout />}>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Route>
+
+      {/* After login */}
+      <Route path="/home" element={<Homepage />} />
+    </Routes>
   );
 };
 
